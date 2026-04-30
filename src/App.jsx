e43f733f7cabd0643d2a985722f53c8f9fa99b29
pblo <pdiazarenas541@gmail.com>
2026-04-30 01:31:59 +0100

    add map add keydown layout edit

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,7 @@ const App = () => {
 
   useEffect(() => {
     Swal.fire({
-      title:
-        "This is Weatherson, to play with weather and locations",
+      title: "This is Weatherson, to play with weather and locations",
       showClass: {
         popup: `
       animate__animated
@@ -39,7 +38,7 @@ const App = () => {
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
       );
       const data = await res.json();
-  /*     console.log(data); */
+      /*     console.log(data); */
       if (data.cod === 200) {
         setWeather({
           name: data.name,
@@ -52,6 +51,7 @@ const App = () => {
           icon: data.weather[0].icon,
           lon: data.coord.lon,
           lat: data.coord.lat,
+          isCountry: data.name.toLowerCase() === city.toLowerCase(),
         });
       } else {
         Swal.fire({
@@ -97,66 +97,82 @@ const App = () => {
     }
   };
 
- const today = new Date().toLocaleDateString('en-US', {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-});
+  const today = new Date().toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
   return (
     <>
-    {/* Background */}
-<div  style={{
-    backgroundImage: `url('https://pdr-assets.b-cdn.net/collections/maps-from-geographicus/745px-1739_Bretez_-_Turgot_View_and_Map_of_Paris2C_France_28c._1900_Taride_issue29_-_Geographicus_-_Paris-turgot-1909.jpg?fit=max&w=2400')`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    position: 'absolute',
-    inset: 0,
-    zIndex: -2,
-  }}>
-    <div  style={{
-    backgroundColor: "#2e1d8a", 
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    position: 'absolute',
-    inset: 0,
-    zIndex: -1,
-    opacity: .8,
-  }}></div>
-        <h1 className="text-start px-5  text-white  font-bold py-1 hidden md:block">Weatherson - {today} </h1>
-      <div className="flex flex-col md:flex-row justify-evenly items-center  m-3 ">
-        <div className="flex flex-col items-center justify-center align-center">
-          <div className="border border-stone-400/40  bg-indigo-400/50 z-0 rounded-2xl p-2 mt-1 shadow-2xl">
-            <div className=" flex justify-center items-center md:mt-5">
+      {/* Background */}
+      <div
+        style={{
+          backgroundImage: `url('https://pdr-assets.b-cdn.net/collections/maps-from-geographicus/745px-1739_Bretez_-_Turgot_View_and_Map_of_Paris2C_France_28c._1900_Taride_issue29_-_Geographicus_-_Paris-turgot-1909.jpg?fit=max&w=2400')`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          position: "fixed",
+          inset: 0,
+          zIndex: -2,
+        }}
+      />
+      <div
+        style={{
+          backgroundColor: "#2e1d8a",
+          backgroundSize: "contain",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          position: "fixed",
+          inset: 0,
+          zIndex: -1,
+          opacity: 0.8,
+        }}
+      />
+      <h1 className="text-start px-5  text-white  font-bold py-1 hidden md:block">
+        Weatherson - {today}{" "}
+      </h1>
+      <div className="flex flex-col md:flex-row justify-evenly items-start md:m-3 min-h-screen">
+        <div className="flex flex-col items-center justify-center align-center mx-auto">
+          <div className="border border-stone-400/40  bg-indigo-400/50 z-0 rounded-2xl mt-1 shadow-2xl md:px-15">
+            <div className=" flex justify-center items-center md:mt-5 p-2 pt-5 ">
               <input
                 className=" h-10 border-gray-300 border rounded-2xl mr-10 pl-4 bg-white"
                 type="text"
                 placeholder="Pick a city"
                 value={city}
                 onChange={(e) => setCity(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    fetchWeather();
+                    setCity("");
+                  }
+                }}
               />
               <button
                 className=" bg-gray-900/30 border hover:border-amber-400 rounded-2xl px-3 h-10 text-white text-xl not-only:font-bold transition-all duration-300 shadow-md hover:bg-indigo-500 hover:px-4 hover:animate-pulse"
-                onClick={fetchWeather}
+                onClick={() => {
+                  fetchWeather();
+                  setCity("");
+                }}
               >
                 Search
               </button>
             </div>
 
             {weather ? (
-              <div className="border border-indigo-500/60 rounded-3xl m-3 flex flex-col md:mt-10 hover:bg-indigo-500/60 hover:text-white hover:px-2 hover:md:px-20 transition-all duration-600 shadow-2xl">
-                <div className="flex jsutify-evenly items-center mx-auto pt-2">
+              <div className="border text-white border-indigo-500/60 rounded-3xl m-3 flex flex-col md:mt-10 hover:bg-indigo-500/60 hover:text-white hover:px-2 hover:md:px-20 transition-all duration-600 shadow-2xl">
+                <div className="flex justify-evenly items-center mx-auto pt-2">
                   {" "}
                   <a
                     className="p-3"
                     href={
-                      weather
-                        ? `https://www.google.com/maps?q=${weather.lat},${weather.lon}`
-                        : `https://en.wikipedia.org/wiki/${name}`
+                      weather.isCountry
+                        ? `https://en.wikipedia.org/wiki/${name}`
+                        : `https://www.google.com/maps?q=${weather.lat},${weather.lon}`
                     }
                     target="_blank"
+                    rel="noreferrer"
                   >
                     <h3 className="font-bold text-2xl text-center hover:text-indigo-200 transition-all duration-400">
                       {weather.name}
@@ -178,7 +194,7 @@ const App = () => {
                   />
                 </div>
 
-                <div className="text-xl font-bold flex flex-col gap-4 text-start mx-auto">
+                <div className=" text-lg md:text-xl font-bold flex flex-col gap-4 text-start mx-auto">
                   <p>Today: {weather.description}</p>
                   <p> Temp: {weather.temp} °C</p>
                   <p>Feels like: {weather.sensation} °C</p>
@@ -186,11 +202,11 @@ const App = () => {
                   <p>Wind: {weather.wind}m/s</p>
                 </div>
 
-                <div className="p-3">
+                <div className="md:p-3 flex justify-center items-center">
                   <iframe
-                    className="w-75 h-75 m-10"
-                    src={`https://www.google.com/maps?q=${weather?.lat},${weather?.lon}&z=12&t=k&output=embed`}
-                    style={{ border: "0", borderRadius: "100%" }}
+                    className="m-10 w-50 h-50 hover:w-100 hover:h-100 tarnsition-all duration-500"
+                    src={`https://www.google.com/maps?q=${weather?.lat},${weather?.lon}&z=1&t=k&output=embed`}
+                    style={{ border: "0", borderRadius: "5%" }}
                     allowFullScreen=""
                     loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"
@@ -198,16 +214,15 @@ const App = () => {
                 </div>
               </div>
             ) : (
-              <div className="hover:animate-pulse border border-stone-400/80 bg-indigo-300 border-3xl mt-10 p-10 rounded-xl">
+              <div className="hover:animate-pulse border border-stone-400/80 bg-indigo-300 border-3xl mt-15 p-10 m-5 md:m-3 rounded-xl flex justify-center">
                 <h3 className="text-2xl text-center text-white font-bold">
                   Hawaii perhaps?
-                </h3> 
+                </h3>
               </div>
             )}
           </div>
         </div>
       </div>
-</div>
     </>
   );
 };
